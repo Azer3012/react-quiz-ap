26b4ae5e2698bfeffe224a044fd47d4c94ff80f8
Azer3012 <azer0297206@gmail.com>
2021-06-09 22:36:18 +0400

    quiz

--- src/components/quiz/Quiz.js
+++ src/components/quiz/Quiz.js
@@ -1,56 +1,67 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { Context } from "../context/Context";
 
-import './css/quiz.css'
+import "./css/quiz.css";
 function Quiz() {
   const { quiz } = useContext(Context);
 
   const [index, setIndex] = useState(0);
   const [suallar, setSual] = useState(quiz.data);
 
-  const [correctAnswer,setCorrectAnswer]=useState(0)
+  const [correctAnswer, setCorrectAnswer] = useState(0);
 
-  const [selectedAnswer,setSelectedAnswer]=useState("")
-
-  let last=suallar.length;
-
+  const [selectedAnswer, setSelectedAnswer] = useState("");
 
   console.log(selectedAnswer);
 
-  const answer =(selected)=>{
-    selected&&last<=3&&setIndex(index+1)
-    setSelectedAnswer("")
-    suallar[index].answer===selected&&setCorrectAnswer(correctAnswer+1)
-    checkAnswer(index)
-    
-  }
-
-  console.log(correctAnswer);
-
-  const checkAnswer=(num)=>{
-    if(num>last){
-        console.log(correctAnswer)
+  const answer = (selected) => {
+    if (selected) {
+      if (index < suallar.length) {
+        setIndex(index + 1);
+      }
+     
     }
-   
-  }
 
-  
+    setSelectedAnswer("");
+    suallar[index].answer === selected && setCorrectAnswer(correctAnswer + 1);
+  };
 
   return (
     <div className="quiz">
-      <h1>{suallar[index].question}</h1>
+      <h1>
+        {index > suallar.length-1? "suallar bitdi" : suallar[index].question}
+      </h1>
 
+      {index < suallar.length?(
+          suallar[index].choices.map((choice) => (
+            <label className="mr-5" key={choice} htmlFor={choice}>
+              {choice}
+              <input
+                className="form-control mr-2"
+                name="rehman"
+                onChange={(e) => setSelectedAnswer(e.target.value)}
+                key={choice}
+                type="radio"
+                id={choice}
+                value={choice}
+              />
+            </label>
+          ))
 
-      {suallar[index].choices.map((choice) => (
+      ):(
+          <p>Duzgun cavablarin sayi: {correctAnswer}</p>
+      )}
+        
 
-        <label className="mr-5" key={choice} htmlFor={choice} >
-          {choice}
-          <input className="form-control mr-2" name="rehman" onChange={(e)=>setSelectedAnswer(e.target.value)} key={choice} type="radio" id={choice} value={choice} />
-        </label>
-      ))}
+      {index<suallar.length?(<button
+        className="btn btn-success col-md-4 mt-5"
+        onClick={() => answer(selectedAnswer)}
+      >
+        next
+      </button>):(
+          <button className="btn btn-success col-md-4 mt-5" >Geriye qayit</button>
+      )}
 
-      <button className="btn btn-success col-md-4 mt-5" onClick={()=>answer(selectedAnswer)}>next</button>
-      
       
     </div>
   );
